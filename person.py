52a1b7e130a53861b7e3de9f7b64c2a64e8bf24b
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-20 09:15:02 +0300

    person inheritance

--- person.py
+++ person.py
@@ -1,1 +1,21 @@
 # person inheritance
+from abc import ABC, abstractmethod
+
+class Person(ABC):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    @abstractmethod
+    def walk(self):
+        return f"walk normally {self.name} of {self.age} years"
+    
+    
+class Athlete(Person):
+    def walk(self):
+        return f"walk faster {self.name} of {self.age} years"
+    
+    
+person1 = Athlete("frank", 22)
+
+print(person1.walk())
