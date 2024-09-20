e980aaf38763503b366ee51569a1ceb0b43734c3
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-20 09:57:45 +0300

    Duck multiple inheritance

--- Flyable.py
+++ Flyable.py
@@ -1,2 +1,20 @@
-# Flyable multiple inheritance
+# Duck multiple inheritance
 
+class Flyable:
+    def fly(self):
+        print("Flying...")
+        
+class Swimmable:
+    def swim(self):
+        print("Swimming...")
+        
+class Duck(Flyable, Swimmable):
+    # No need to override if you're not modifying the behavior
+    pass
+
+# Creating an instance of Duck
+duck1 = Duck()
+
+# Calling inherited methods
+duck1.swim()
+duck1.fly()
